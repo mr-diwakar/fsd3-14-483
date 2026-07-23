f0fc00c0cae0c96332f35aa57be39b89e9c29bd3
Diwakar jha <diwakarjha0810@gmail.com>
2026-07-23 16:25:33 +0530

    new tasks

---
+++ Lab1/task1.js
@@ -0,0 +1,28 @@
+const f1 = () => {
+  console.log("f1 starts");
+  f2();
+  console.log("f1 running");
+  console.log("f1 end");
+};
+const f2 = () => {
+  console.log("f2 starts");
+  f3();
+  console.log("f2 running");
+  console.log("f2 ends");
+};
+const f3 = () => {
+  console.log("f3 starts");
+  console.log("f3 running");
+  console.log("f3 end");
+};
+function main() {
+  console.log("main");
+  f1();
+  console.log("end main");
+}
+main();
+//asynchronous call using timers 
+//1.setTimeout
+//2.setImmeidate
+//3.process.nextTick
+//4. setInternal
