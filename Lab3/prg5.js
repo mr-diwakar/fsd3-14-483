ead88b33cc98595ea59de1b1f2fe2d863eb93d45
Diwakar jha <diwakarjha0810@gmail.com>
2026-08-27 13:43:12 +0530

    lab3 stream starts

---
+++ Lab3/prg5.js
@@ -0,0 +1,14 @@
+import http from "http";
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "content-type": "application/json" });
+  const product = {
+    name: "Mobile",
+    price: 25000,
+    discount: "10%",
+    company: "Samsung",
+  };
+  res.end(JSON.stringify(product));
+});
+
+server.listen(3000, () => console.log("server is running..."));
